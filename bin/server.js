ae4f414892604ab20d43a85b331abd19a560038f
Gildas Vinsot <givinsot@microsoft.com>
2016-11-15 10:26:53 +0100

    Fix for web app

--- bin/server.js
+++ bin/server.js
@@ -5,6 +5,6 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     app_1.OpenLaw.App.Run();
     res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ http.createServer(function (req:any, res:any) {
 
     res.end('Hello World\n');
 
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT || 8080);
 
 
 
-console.log('Server running at http://127.0.0.1:1337/');  
+console.log('Server running at http://localhost:8080/');  
