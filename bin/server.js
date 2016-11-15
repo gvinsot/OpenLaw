d453551c6be92feba6d2e01dd4695c5afbbe5703
Gildas Vinsot <givinsot@microsoft.com>
2016-11-15 10:48:45 +0100

    Forgot to build...

--- bin/server.js
+++ bin/server.js
@@ -5,6 +5,6 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     app_1.OpenLaw.App.Run();
     res.end('Hello World\n');
-}).listen(8080, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(process.env.PORT || 8080);
+console.log('Server running at http://localhost:8080/');
 //# sourceMappingURL=server.js.map
